f701d9e90ec302db02c8f44125f6afd2a5342f18
Hovo-93 <ovo.aroyan@mail.ru>
2022-03-13 22:47:06 +0300

    lesson3

---
+++ to/serializers.py
@@ -0,0 +1,19 @@
+from to.models import Project, Todo
+from rest_framework.serializers import ModelSerializer
+from users.serializers import UserModelSerializer
+
+
+class ProjectModelSerializer(ModelSerializer):
+    users = UserModelSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class TodoModelSerializer(ModelSerializer):
+    user = UserModelSerializer()
+
+    class Meta:
+        model = Todo
+        fields = '__all__'
--- to/views.py
+++ to/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from to.models import Project, Todo
+from to.serializers import ProjectModelSerializer, TodoModelSerializer
+
 
 # Create your views here.
+class ProjectModelViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class TodoModelViewSet(ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoModelSerializer
