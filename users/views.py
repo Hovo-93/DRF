3a137accba5a99a43118700849448c19ce11e7e2
Hovo-93 <ovo.aroyan@mail.ru>
2022-03-18 17:12:16 +0300

    added class for pagination end change UserViews

--- users/views.py
+++ users/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from users.models import User
 from users.serializers import UserModelSerializer
+from rest_framework import mixins, viewsets
 
 
-# Create your views here.
-class UserModelViewSet(ModelViewSet):
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 2
+
+
+class UserCustomViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    filterset_fields = ['uid']
+    pagination_class = LargeResultsSetPagination
+
+# Create your views here.
+# class UserModelViewSet(ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserModelSerializer
