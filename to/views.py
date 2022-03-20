c2ef8e4e754cd44a5bf573ad5cbcef71b91c3013
Hovo-93 <ovo.aroyan@mail.ru>
2022-03-20 15:45:00 +0300

    added pagination and filters (lesson4)

--- to/views.py
+++ to/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from to.models import Project, Todo
 from to.serializers import ProjectModelSerializer, TodoModelSerializer
+
+
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 10
+
+
+class MaxResultsSetPagination(PageNumberPagination):
+    page_size = 20
 
 
 # Create your views here.
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    filterset_fields = ['title']
+    pagination_class = LargeResultsSetPagination
 
 
 class TodoModelViewSet(ModelViewSet):
-    queryset = Todo.objects.all()
+    queryset = Todo.objects.all().order_by('id')
     serializer_class = TodoModelSerializer
+    filterset_fields = ['project']
+    pagination_class = MaxResultsSetPagination
+
+    def perform_destroy(self, instance):
+        instance.status = 'CLS'
+        instance.save()
