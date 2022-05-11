1d0b9e9997f15cca791112ff955f5bf5aecce235
Hovo-93 <ovo.aroyan@mail.ru>
2022-05-11 16:41:49 +0300

    added docker

--- service/settings.py
+++ service/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'service.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'PASSWORD': '1',
+        'USER': 'HH',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
